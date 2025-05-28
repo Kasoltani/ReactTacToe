824197e100040f9c40e9ffab29bf75a1f5718b1c
kasoltani <kassoltani@gmail.com>
2025-05-27 17:47:51 -0700

    adding backend functionality

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello'
+
+@app.route('/setGameState', methods=['POST'])
+def set_game_state():
+    print("test")
 
 if __name__ == "__main__":
     app.run(debug=True) 
